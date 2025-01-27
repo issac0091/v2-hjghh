2f427baa86c90c392d9336110e4b8c92a7e1c760
issac0091 <yangchennk@mail.nankai.edu.cn>
2025-01-27 16:55:39 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,17 +5,17 @@ if (!(input.buttonIsPressed(Button.A))) {
 }
 basic.forever(function () {
     if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.Black) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.Black)) {
-        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, 50)
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, 200)
     }
     if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.Black) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.White)) {
         cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Left_Z_Motor, 0)
-        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_Z_Motor, 100)
+        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_Z_Motor, 255)
     }
     if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.White) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.Black)) {
         cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_Z_Motor, 0)
-        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Left_Z_Motor, 100)
+        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Left_Z_Motor, 255)
     }
     if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.White) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.White)) {
-        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Back, 50)
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Back, 200)
     }
 })
