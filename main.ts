e6d9ad428c8437bf0d091eae3654b8019e2100e1
issac0091 <yangchennk@mail.nankai.edu.cn>
2025-01-27 20:52:29 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,17 +7,17 @@ basic.forever(function () {
     basic.showIcon(IconNames.Heart)
     basic.showIcon(IconNames.SmallHeart)
     if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.Black) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.Black)) {
-        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, 200)
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, 50)
     }
     if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.Black) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.White)) {
         cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_Z_Motor, 0)
-        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_Z_Motor, 255)
+        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_Z_Motor, 100)
     }
     if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.White) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.Black)) {
         cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_Z_Motor, 0)
-        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_Z_Motor, 255)
+        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_Z_Motor, 100)
     }
     if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.White) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.White)) {
-        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Back, 200)
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Back, 50)
     }
 })
