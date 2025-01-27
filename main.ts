86f1321f8582c76470cecd121e00ef17bf7978e4
issac0091 <yangchennk@mail.nankai.edu.cn>
2025-01-27 21:33:55 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,18 +6,18 @@ if (!(input.buttonIsPressed(Button.A))) {
 basic.forever(function () {
     basic.showIcon(IconNames.Heart)
     basic.showIcon(IconNames.SmallHeart)
-    if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.Black) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.Black)) {
-        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, 50)
+    if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.Black) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.Black)) {
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, 200)
     }
-    if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.Black) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.White)) {
-        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_Z_Motor, 0)
-        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_Z_Motor, 100)
+    if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.Black) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.White)) {
+        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Left_F_Motor, 50)
+        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_Z_Motor, 255)
     }
-    if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.White) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.Black)) {
-        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_Z_Motor, 0)
-        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_Z_Motor, 100)
+    if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.White) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.Black)) {
+        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_F_Motor, 50)
+        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Left_Z_Motor, 255)
     }
-    if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.White) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.White)) {
-        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Back, 50)
+    if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.White) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.White)) {
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Back, 200)
     }
 })
